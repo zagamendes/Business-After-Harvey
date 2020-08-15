ee55adeda219d8ac5b75bb5edd09dc14b8be5a05
zagamendes <30512726+zagamendes@users.noreply.github.com>
2020-08-15 13:18:23 -0300

    Update connection.php

--- classes/connection.php
+++ classes/connection.php
@@ -1,9 +1,9 @@
 <?php
 	class Connection{
-		private static $host="localhost";
-		private static $user="root";
-		private static $password="vertrigo";
-		private static $data_base="harvey";
+		private static $host="cdm1s48crk8itlnr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+		private static $user="uageuej2c378lp56";
+		private static $password="d290e7qu7zq9w5ii";
+		private static $data_base="rlxwewubb9x4xj8d";
 
 		public static function connect_db(){
 			$connection=@mysqli_connect(self::$host,self::$user,self::$password,self::$data_base);
